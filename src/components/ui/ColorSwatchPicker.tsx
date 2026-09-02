8676f8c1bce7427d867952684779fc7a0621fe9f
RykoTheDev <rayyanaziz76@gmail.com>
2026-09-03 03:56:02 +0500

    fix typescript issue

--- src/components/ui/ColorSwatchPicker.tsx
+++ src/components/ui/ColorSwatchPicker.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { IconCheck } from '../../lib/icons'
 import { HsvColorPicker } from './HsvColorPicker'
 import { Dropdown } from './Dropdown'
